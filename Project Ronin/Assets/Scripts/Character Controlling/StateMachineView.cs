f17caf189004d847d41106f19ef7ac905f69640a
Xiangyu Wan <wonjohnny@outlook.com>
2022-01-26 12:06:16 -0800

    setting a new statemachine interface

--- Project Ronin/Assets/Scripts/Character Controlling/StateMachineView.cs
+++ Project Ronin/Assets/Scripts/Character Controlling/StateMachineView.cs
@@ -8,13 +8,21 @@ public class StateMachineView : DialogueViewBase
 {
     public DialogueRunner dialogueRunner;
 
+    // active transitions
+    // TODO: implement transition class
+
     private void Awake()
     {
-        dialogueRunner.AddCommandHandler<string, float>("change_float", HandleChangeFloat);
-        dialogueRunner.AddCommandHandler<string, float>("set_float", HandleSetFloat);
-        dialogueRunner.AddCommandHandler<string>("add_tag", HandleTempTag);
-        dialogueRunner.AddCommandHandler<string, float>("add_temp_tag", HandleAddTag);
-        dialogueRunner.AddCommandHandler<string>("remove_tag", HandleRemoveTag);
+        //dialogueRunner.AddCommandHandler<string, float>("change_float", HandleChangeFloat);
+        //dialogueRunner.AddCommandHandler<string, float>("set_float", HandleSetFloat);
+        //dialogueRunner.AddCommandHandler<string>("add_tag", HandleTempTag);
+        //dialogueRunner.AddCommandHandler<string, float>("add_temp_tag", HandleAddTag);
+        //dialogueRunner.AddCommandHandler<string>("remove_tag", HandleRemoveTag);
+    }
+
+    private void Start()
+    {
+        // TODO: initialte runner
     }
 
     // TODO: implement all those
@@ -24,38 +32,90 @@ public class StateMachineView : DialogueViewBase
         base.DialogueStarted();
     }
 
-    public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+    public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected)
     {
-        base.RunLine(dialogueLine, onDialogueLineFinished);
+        // TODO: register callback
     }
 
-    public override void NodeComplete(string nextNode, Action onComplete)
+
+    // condition objects used for evaluation
+    private interface Condition
     {
-        base.NodeComplete(nextNode, onComplete);
+        bool Evaluate();
     }
 
-    private void HandleChangeFloat(string attr, float delta)
+    private class SimpleCondition : Condition
     {
+        Func<bool> condition;
 
+        public bool Evaluate()
+        {
+            return condition();
+        }
+    }
+    
+    private class AndClause : Condition
+    {
+        public List<Condition> conditions = new List<Condition>();
+
+        public bool Evaluate()
+        {
+            bool result = true;
+
+            foreach (Condition condition in conditions)
+            {
+                result &= condition.Evaluate();
+            }
+
+            return result;
+        }
     }
 
-    private void HandleSetFloat(string attr, float newValue)
+    private class OrClause : Condition
     {
+        public List<Condition> conditions = new List<Condition>();
 
+        public bool Evaluate()
+        {
+            bool result = true;
+
+            foreach (Condition condition in conditions)
+            {
+                result |= condition.Evaluate();
+            }
+
+            return result;
+        }
     }
 
-    private void HandleTempTag(string tag)
+    // handlers building the 
+    private void HandleSetState(string nodeName)
     {
-
+        throw new NotImplementedException();
     }
 
-    private void HandleAddTag(string tag, float duration)
+    private void HandleAddTransition()
     {
-
+        throw new NotImplementedException();
     }
 
-    private void HandleRemoveTag(string tag)
+    private void HandleAndOpen()
     {
+        throw new NotImplementedException();
+    }
 
+    private void HandleAndClose()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleOrOpen()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleOrClose()
+    {
+        throw new NotImplementedException();
     }
 }
