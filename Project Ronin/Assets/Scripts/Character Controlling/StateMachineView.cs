91441bd93db45dc93230c10daa8bdaea9ea4ab14
Xiangyu Wan <wonjohnny@outlook.com>
2022-01-26 14:35:36 -0800

    implemented a simple conditional control language in yarnspinner

--- Project Ronin/Assets/Scripts/Character Controlling/StateMachineView.cs
+++ Project Ronin/Assets/Scripts/Character Controlling/StateMachineView.cs
@@ -4,61 +4,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
 
+// the best documentation is naming
+
 public class StateMachineView : DialogueViewBase
 {
     public DialogueRunner dialogueRunner;
 
-    // active transitions
-    // TODO: implement transition class
-
-    private void Awake()
-    {
-        //dialogueRunner.AddCommandHandler<string, float>("change_float", HandleChangeFloat);
-        //dialogueRunner.AddCommandHandler<string, float>("set_float", HandleSetFloat);
-        //dialogueRunner.AddCommandHandler<string>("add_tag", HandleTempTag);
-        //dialogueRunner.AddCommandHandler<string, float>("add_temp_tag", HandleAddTag);
-        //dialogueRunner.AddCommandHandler<string>("remove_tag", HandleRemoveTag);
-    }
-
-    private void Start()
-    {
-        // TODO: initialte runner
-    }
-
-    // TODO: implement all those
-
-    public override void DialogueStarted()
-    {
-        base.DialogueStarted();
-    }
-
-    public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected)
-    {
-        // TODO: register callback
-    }
-
-
     // condition objects used for evaluation
-    private interface Condition
+    private abstract class Condition
     {
-        bool Evaluate();
+        public bool done = false;
+        public abstract bool Evaluate();
+        public virtual void Include(List<Condition> includee)
+        {
+            throw new NotImplementedException("Include called upon simple condition!");
+        }
     }
 
     private class SimpleCondition : Condition
     {
         Func<bool> condition;
 
-        public bool Evaluate()
+        SimpleCondition() { done = true; }
+
+        public override bool Evaluate()
         {
             return condition();
         }
     }
-    
+
     private class AndClause : Condition
     {
         public List<Condition> conditions = new List<Condition>();
 
-        public bool Evaluate()
+        public override bool Evaluate()
         {
             bool result = true;
 
@@ -69,13 +48,18 @@ public class StateMachineView : DialogueViewBase
 
             return result;
         }
+
+        public override void Include(List<Condition> includee)
+        {
+            conditions.AddRange(includee);
+        }
     }
 
     private class OrClause : Condition
     {
         public List<Condition> conditions = new List<Condition>();
 
-        public bool Evaluate()
+        public override bool Evaluate()
         {
             bool result = true;
 
@@ -86,36 +70,86 @@ public class StateMachineView : DialogueViewBase
 
             return result;
         }
+
+        public override void Include(List<Condition> includee)
+        {
+            conditions.AddRange(includee);
+        }
     }
 
-    // handlers building the 
-    private void HandleSetState(string nodeName)
+    // active transitions
+    List<Condition> activeTransitions = new List<Condition>();
+    Action<int> currentCallback = null;
+    Stack<Condition> conditionBuilder = new Stack<Condition>();
+
+    private void Awake()
     {
-        throw new NotImplementedException();
+        dialogueRunner.AddCommandHandler("transition", HandleAddTransitionOpen);
+        dialogueRunner.AddCommandHandler("/", HandleClose);
+        dialogueRunner.AddCommandHandler("and", HandleAndOpen);
+        dialogueRunner.AddCommandHandler("or", HandleOrOpen);
     }
 
-    private void HandleAddTransition()
+    private void Start()
     {
-        throw new NotImplementedException();
+        // TODO: initialize runner
+    }
+
+    public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected)
+    {
+        currentCallback = onOptionSelected;
+    }
+
+    public override void NodeComplete(string nextNode, Action onComplete)
+    {
+        // TODO: load next state (node name is the key to states)
+        base.NodeComplete(nextNode, onComplete);
+    }
+
+    // handlers building the -
+    private void HandleAddTransitionOpen()
+    {
+        // seems there's nothing to do
+        // logically this line cleans up any state from the previous transition
+        conditionBuilder.Clear();
+    }
+
+    private void HandleClose()
+    {
+        List<Condition> closedConditions = new List<Condition>();
+
+        while (conditionBuilder.Count > 0 && conditionBuilder.Peek().done)
+        {
+            closedConditions.Add(conditionBuilder.Pop());
+        }
+
+        if (conditionBuilder.Count > 0) // closing a clause
+        {
+            conditionBuilder.Peek().Include(closedConditions);
+            conditionBuilder.Peek().done = true;
+        }
+        else // closing a transition
+        {
+            if (closedConditions.Count > 1)
+            {
+                activeTransitions.AddRange(closedConditions);
+            }
+            else
+            {
+                AndClause newTransition = new AndClause();
+                newTransition.Include(closedConditions);
+                activeTransitions.Add(newTransition);
+            }
+        }
     }
 
     private void HandleAndOpen()
     {
-        throw new NotImplementedException();
-    }
-
-    private void HandleAndClose()
-    {
-        throw new NotImplementedException();
+        conditionBuilder.Push(new AndClause());
     }
 
     private void HandleOrOpen()
     {
-        throw new NotImplementedException();
-    }
-
-    private void HandleOrClose()
-    {
-        throw new NotImplementedException();
+        conditionBuilder.Push(new OrClause());
     }
 }
