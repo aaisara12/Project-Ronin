9c6460e0409774066de17b31b76702c3c654c73f
RyanVuong <mail2rv2300@gmail.com>
2022-02-03 01:51:53 -0800

    fixed player brain to enable smooth movement

--- Project Ronin/Assets/Scripts/PlayerBrain.cs
+++ Project Ronin/Assets/Scripts/PlayerBrain.cs
@@ -8,29 +8,50 @@ public class PlayerBrain : MonoBehaviour
     private CharacterCaptureController captureController;
     [SerializeField] Interactor interactor;
     private Rigidbody rb;
-    void Awake(){
+    private Player_Controls playerControls;
+    private void Awake(){
         rb = GetComponent<Rigidbody>();
         captureController = GetComponent<CharacterCaptureController>();
-    }
-    void OnMove(InputValue movementVal){
-
-        Vector2 inputVector = movementVal.Get<Vector2>();
-        Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
-        captureController?.MoveInDirection(movementVector);
-        // Debug.Log("Movement Vector:" + movementVector);
+        playerControls = new Player_Controls();
     }
 
-    void OnDash(){
+    private void OnEnable(){
+        playerControls.Enable();
+    }
+
+    private void OnDisable(){
+        playerControls.Disable();
+    }
+    
+    // private void OnMove(InputValue movementVal){
+
+    //     Vector2 inputVector = movementVal.Get<Vector2>();
+    //     Vector3 movementVector = new Vector3(inputVector.x, 0, inputVector.y);
+    //     captureController?.MoveInDirection(movementVector);
+    //     // Debug.Log("Movement Vector:" + movementVector);
+    // }
+
+    private void OnDash(){
         Vector3 dashDirection = new Vector3(transform.forward.x, 0, transform.forward.z);
         captureController?.DashInDirection(dashDirection);
-        // Debug.Log(dashDirection);
+        Debug.Log(dashDirection);
     }
 
-    void OnPause(){
+    private void OnPause(){
         // Enter pause menu
     }
 
-    void OnInteract(){
+    private void OnInteract(){
         interactor.InteractWithNearestInRange();
+    }
+    
+
+    private void movePlayer(){
+        Vector2 move = playerControls.Player.Move.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(move.x, 0, move.y);
+        captureController?.MoveInDirection(movementVector);
+    }
+    private void Update(){
+        movePlayer();
     }
 }
