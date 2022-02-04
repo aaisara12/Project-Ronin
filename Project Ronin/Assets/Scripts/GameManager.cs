1d423b9f63ae0954a2f3da848f934102e27dea18
aaisara12 <aisara121201@gmail.com>
2022-02-04 00:26:56 -0800

    Added pause events
    Pause events will let other programs know the game has changed state and can react accordingly (we don't want to set timescale to 1 because it will affect animations and limits what we can do during the pause state)

--- Project Ronin/Assets/Scripts/GameManager.cs
+++ Project Ronin/Assets/Scripts/GameManager.cs
@@ -6,31 +6,43 @@ public class GameManager : MonoBehaviour
 {
 
     public event System.Action OnPauseGame;
+    public event System.Action<bool> OnNewPauseState;   // More compact pause event (use in future sprints)
 
     public static GameManager Instance {get; private set;}
 
+    bool isPaused = false;
+
     void Awake()
     {
-        Instance = this;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
     }
 
     /// <summary> Freezes all game objects in scene </summary>
     public void PauseGame()
     {
-        OnPauseGame?.Invoke();
+        SetPauseState(true);
+    }
 
-        // TODO
+    // This is a more flexible public method for setting pause state
+    // that will be transitioned to in a future sprint
+    public void SetPauseState(bool shouldPause)
+    {
+        if(!isPaused && shouldPause)
+        {
+            isPaused = true;
+            OnPauseGame?.Invoke();
+            OnNewPauseState?.Invoke(isPaused);
+        }
+        else if(isPaused && !shouldPause)
+        {
+            isPaused = false;
+            OnNewPauseState?.Invoke(isPaused);
+        }
     }
 }
