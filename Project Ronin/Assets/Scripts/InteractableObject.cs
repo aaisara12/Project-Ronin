cfbfe2a020fc7efce5bd60d4f494e6d2438edaf6
Daniel Jaffe <daniel.jaffe@g.ucla.edu>
2022-02-05 06:25:19 -0800

    Ticket temporarily blocked
    Final implementation depends on design decisions to be discussed at the programming meeting today.

--- Project Ronin/Assets/Scripts/InteractableObject.cs
+++ Project Ronin/Assets/Scripts/InteractableObject.cs
@@ -1,14 +1,35 @@
+using System;
 using UnityEngine;
 
-public abstract class InteractableObject : MonoBehaviour
+public class InteractableObject : MonoBehaviour
 {
-    [SerializeField] private bool isInteractable = false;
+    [SerializeField] private string objectName = "";
+    [SerializeField] private bool isInteractable = true;
+    [SerializeField] private float interactionRange = 3.0f;
 
-    // Use this function to define desired behavior on interaction
-    protected abstract void DoInteraction();
+    /// <summary>Event function to ensure the object has a name</summary>
+    /// <remarks>Uses the gameObject name if objectName is empty</remarks>
+    private void Awake()
+    {
+        if (objectName == "")
+        {
+            objectName = gameObject.name;
+        }
+    }
 
-    // Use this function to initiate interaction
-    /// <summary>Try to interact with this object, return whether interaction was successful</summary>
+
+    /// <summary>Virtual function that defines behavior on interaction</summary>
+    /// <remarks>Function must be overriden as no default behavior is defined</remarks>
+    protected virtual void DoInteraction()
+    {
+        DisplayMessage();
+        Debug.LogFormat("{0} has been interacted with!", gameObject.name);
+    }
+
+
+    /// <summary>Attempt to interact with this object</summary>
+    /// <returns>true on success</returns>
+    /// <remarks>Object might be an interactableObject but not yet interactable in scene</remarks>
     public bool TryInteract()
     {
         if (isInteractable)
@@ -20,10 +41,50 @@ public abstract class InteractableObject : MonoBehaviour
         return false;
     }
 
-    // Use to enable or disable interaction
-    /// <summary>Make object interactable if <paramref name = "state"/> is true, and false otherwise</summary>
+    /// <summary>Display interaction text to screen</summary>
+    /// <remarks>objectName must == filename</remarks>
+    private void DisplayMessage()
+    {
+        // Stream in the associated file for input text if it exists
+        string interactionText;
+        try
+        {
+            interactionText = Application.streamingAssetsPath + "/InteractableObject/" + objectName + ".txt";
+        }
+        catch (Exception e)
+        {
+            interactionText = "";
+            Debug.LogFormat("{0}.txt not found!", objectName);
+        }
+
+        // Display the text to the UI -- Using GameManager?
+        GameManager gm = GameManager.Instance;
+        if (gm != null && interactionText != "")
+        {
+            Debug.LogFormat("Interaction text: {0}", interactionText);
+            //TODO: This function should either utilize the GameManager or a UIManager of some sort.
+            // Questions:
+            // 1) What object in scene will MainTextUI guarantee to be attached to? Seems like having a manager to
+            //    always reference from would be easiest.
+            // 2) Will we ever run into issues with queues of text? Perhaps an item is triggered or picked up while 
+            //    another popup is already up? Or will these text boxes always temporarily pause the game?
+        }
+    }
+
+
+    /// <summary>Make object interactable</summary>
+    /// <param name = "state">Boolean </param> 
+    /// <remarks>An objects use can be turned on or off via this function.</remarks>
     public void SetInteractable(bool state)
     {
         isInteractable = state;
+    }
+
+
+    /// <summary>Event function to highlight an interactable object in scene</summary>
+    private void OnDrawGizmos()
+    {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(gameObject.transform.position, interactionRange);
     }
 }
