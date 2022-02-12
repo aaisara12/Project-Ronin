026761d286dff2cb3ef2fae978e3fe3eb1793885
Xiangyu Wan <wonjohnny@outlook.com>
2022-02-11 16:25:48 -0800

    integrated movement

--- Project Ronin/Assets/Scripts/Character Controller/States/MoveState.cs
+++ Project Ronin/Assets/Scripts/Character Controller/States/MoveState.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class MoveState : StateMachineBehaviour
 {
+    CharacterCaptureController movementController = null;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        movementController = animator.GetComponent<CharacterCaptureController>();
+
+        if (movementController == null)
+        {
+            throw new MissingComponentException("Animator State Error: Characterr doesn't have CharacterCaptureController component!");
+        }
+    }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector3 movement = new Vector3(animator.GetFloat("xInput"), 0, animator.GetFloat("yInput"));
+
+        movementController.MoveInDirection(movement);
+    }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
