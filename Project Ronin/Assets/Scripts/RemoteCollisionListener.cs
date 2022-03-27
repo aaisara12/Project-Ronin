e865562ab7c33280923dde44ecad06b2ec118f21
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2022-03-27 02:46:22 -0700

    52 animation driven player attack (#67)
    * Added remote collision detection
    
    Remote collision detection (detecting collision from a separate GameObject) is useful for keeping important scripts that react to collisions deep in the hierarchy on the top level of the hierarchy (such as animation-driven moves)
    
    * Replaced ability version of melee attack to animation-driven
    
    * Created custom enemy dummy basic melee hitbox
    
    Enemy dummy does not have sword, so hitbox must be different (must have different configuration for animations)

---
+++ Project Ronin/Assets/Scripts/RemoteCollisionListener.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RemoteCollisionListener : MonoBehaviour
+{
+    // GameObject whose collisions we are listening in on
+    [SerializeField] GameObject remoteCollisionObject;
+
+    CollisionRelay relay;
+
+    void Awake()
+    {
+        if(remoteCollisionObject == null)
+            throw new MissingComponentException("Collision Relay: No collision object specified!");
+
+        relay = remoteCollisionObject.AddComponent<CollisionRelay>();
+        relay.OnMyTriggerEnter += OnTriggerEnterRemote;
+        relay.OnMyCollisionEnter += OnCollisionEnterRemote;
+    }
+
+    void OnDisable()
+    {
+        relay.OnMyTriggerEnter -= OnTriggerEnterRemote;
+        relay.OnMyCollisionEnter -= OnCollisionEnterRemote;
+    }
+    
+    protected virtual void OnCollisionEnterRemote(Collision other){}
+    protected virtual void OnTriggerEnterRemote(Collider other){}
+
+    public class CollisionRelay : MonoBehaviour
+    {
+        public event System.Action<Collision> OnMyCollisionEnter;
+        public event System.Action<Collider> OnMyTriggerEnter;
+
+        void OnTriggerEnter(Collider other)
+        {
+            OnMyTriggerEnter?.Invoke(other);
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            OnMyCollisionEnter?.Invoke(collision);
+        }
+    }
+}
