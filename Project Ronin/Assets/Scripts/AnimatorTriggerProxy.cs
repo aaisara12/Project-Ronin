757112b915735c17506177ff7bb178a0912a4036
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2022-05-15 18:34:56 -0700

    85 spaced triple combo (#93)
    * Fixed attack spamming and improved attack snappiness
    
    * Enlarged sword on first melee for greater clarity of attack
    
    * Generalized attack trigger spam protection to all triggers
    
    * Update roll-ronin.anim
    
    * Updated Ronin prefab to include animator proxy scripts

---
+++ Project Ronin/Assets/Scripts/AnimatorTriggerProxy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class used to prevent animator attack parameter spamming
+public class AnimatorTriggerProxy : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    bool isInputUnlocked = true;
+
+    [SerializeField] int numLocking = 0;
+
+    // Called on particular frames of animation
+    public void ReleaseInputLock()
+    {
+        numLocking--;
+        if(numLocking == 0)
+            isInputUnlocked = true;
+        //Debug.Log("Enabled attack");
+    }
+
+    public void AddInputLock()
+    {
+        numLocking++;
+        isInputUnlocked = false;
+        //Debug.Log("Disabled attack");
+    }
+
+    public void RequestTrigger(string triggerName)
+    {
+        if(isInputUnlocked)
+        {
+            animator.SetTrigger(triggerName);
+        }
+    }
+}
