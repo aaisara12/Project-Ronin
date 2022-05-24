3c866f8c006cb6ce26cd9b31d7059e93aae14123
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2022-05-24 14:01:51 -0700

    104 wave manager (#109)
    * Spawning capabilities
    
    * Merged in new AI changes
    
    Auto-initialize patrol points when enemies spawn
    
    * Added in spawn sound effect among others
    
    * Added spawn animation for enemy
    
    * Added growing health bar animation
    
    * Removed unnecessary timing code
    
    * Added auto-start option
    
    Allow waves to begin manually (so it can be controlled by other scripts)
    
    * Added slow motion kill effect on last minion in wave
    
    * Update WaveManager.unity

---
+++ Project Ronin/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] List<Wave> waves = new List<Wave>();
+    [SerializeField] int currentWave = 0;
+    [SerializeField] bool autoStart = true;
+
+    public event System.Action<int> OnStartWave;
+    public event System.Action<int> OnClearedWave;
+
+    List<HealthStat> unitsSpawned = new List<HealthStat>();
+
+    void Start()
+    {
+        if(autoStart)
+            TrySpawnNextWave();
+    }
+
+
+    public bool TrySpawnNextWave()
+    {
+        if(currentWave < waves.Count)
+        {
+            AudioManager.instance.PlaySound("wave-spawn");
+            currentWave++;
+            OnStartWave?.Invoke(currentWave);
+
+            // Spawn enemy from each spawner location
+            foreach(Spawner s in waves[currentWave - 1].spawners)
+            {
+                HealthStat healthStat = s.Spawn();
+                unitsSpawned.Add(healthStat);
+                healthStat.OnDied += HandleUnitDied;
+            }
+
+            if(waves[currentWave - 1].spawners.Count == 0)
+            {
+                Debug.LogWarning("No spawners specified for wave " + currentWave + "!");
+                TrySpawnNextWave();
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    void HandleUnitDied(HealthStat healthStat)
+    {
+        unitsSpawned.Remove(healthStat);
+        healthStat.OnDied -= HandleUnitDied;
+
+        if(unitsSpawned.Count == 0)
+        {
+            StartCoroutine(SlowKill());
+            OnClearedWave?.Invoke(currentWave);
+            TrySpawnNextWave();
+        }
+    }
+
+    IEnumerator SlowKill()
+    {
+        Time.timeScale = 0.1f;
+        yield return new WaitForSeconds(0.1f);
+        Time.timeScale = 1;
+
+    }
+
+
+}
+
+[System.Serializable]
+public struct Wave
+{
+    public List<Spawner> spawners;
+}
