38335f0ac69d342f86fabc89a48568ecac12ef6c
aaisara12 <aisara121201@gmail.com>
2022-05-25 19:10:12 -0700

    Added enemy spawn during cut scene

--- Project Ronin/Assets/Scripts/OpeningCinematicController.cs
+++ Project Ronin/Assets/Scripts/OpeningCinematicController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,42 @@ using UnityEngine;
 public class OpeningCinematicController : RemoteCollisionListener
 {
     [SerializeField] Animator cinematicAnimator;
+    [SerializeField] CutsceneRelay relay;
+    [SerializeField] PlayerBrain playerBrain;
+    [SerializeField] WaveManager waveManager;
     bool hasTriggeredCinematic = false;
+
+    void OnEnable()
+    {
+        relay.OnCutsceneStart += HandleCutsceneStart;
+        relay.OnCutsceneEnded += HandleCutsceneEnd;
+    }
+
+    void OnDestroy()
+    {
+        relay.OnCutsceneStart -= HandleCutsceneStart;
+        relay.OnCutsceneEnded -= HandleCutsceneEnd;
+    }
+
     protected override void OnTriggerEnterRemote(Collider other)
     {
         if(!hasTriggeredCinematic && other.CompareTag("Player"))
         {
             cinematicAnimator.SetTrigger("start");
             AudioManager.instance.SwapTrack("soundtrack-battle");
+            waveManager.TryStartNextWave();
             hasTriggeredCinematic = true;
         }
         
+    }
+
+    void HandleCutsceneStart()
+    {
+        playerBrain.enabled = false;
+    }
+
+    void HandleCutsceneEnd()
+    {
+        playerBrain.enabled = true;
     }
 }
