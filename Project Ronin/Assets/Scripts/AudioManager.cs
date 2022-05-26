09caf9623e72a6cbe704237616e3badc402d2560
aaisara12 <aisara121201@gmail.com>
2022-05-25 17:05:31 -0700

    Added soundtrack fading capabilities to AudioManager
    Music should fade from peaceful to battle when entering the arena

--- Project Ronin/Assets/Scripts/AudioManager.cs
+++ Project Ronin/Assets/Scripts/AudioManager.cs
@@ -68,6 +68,54 @@ public class AudioManager : MonoBehaviour
         AudioSource.PlayClipAtPoint(GetSoundSource(soundName).clip, location);
     }
 
+    
+    [Header("Soundtrack")]
+    [SerializeField] AudioSource primaryTrackSlot;
+    [SerializeField] AudioSource secondaryTrackSlot;      // Swap in any sound or track to loop
+    [SerializeField] float fadeTime = 0.75f;
+    [SerializeField] float maxVolume = 0.2f;
+
+    bool isPrimaryTrack = true;     // Keep track of which track is the one being heard
+    public void SwapTrack(string soundName)
+    {
+        if(name2source.ContainsKey(soundName))
+        {
+            StopAllCoroutines();    // Prevent multiple fades occurring from player spam
+            StartCoroutine(FadeTrack(name2source[soundName][0].clip));
+            isPrimaryTrack = !isPrimaryTrack;
+        }
+        
+    }
+
+    IEnumerator FadeTrack(AudioClip clip)
+    {
+        float timePassed = 0;
+        if(isPrimaryTrack)
+        {
+            secondaryTrackSlot.clip = clip;
+            secondaryTrackSlot.Play();      // Restart the clip
+            while(timePassed < fadeTime)
+            {
+                primaryTrackSlot.volume = Mathf.Lerp(maxVolume, 0, timePassed/fadeTime);
+                secondaryTrackSlot.volume = Mathf.Lerp(0, maxVolume, timePassed/fadeTime);
+                timePassed += Time.unscaledDeltaTime;       // In case we choose to freeze gameplay with Time.timeScale = 0 but still fade
+                yield return null;
+            }
+        }
+        else
+        {
+            primaryTrackSlot.clip = clip;   // Restart the clip
+            primaryTrackSlot.Play();
+            while(timePassed < fadeTime)
+            {
+                primaryTrackSlot.volume = Mathf.Lerp(0, maxVolume, timePassed/fadeTime);
+                secondaryTrackSlot.volume = Mathf.Lerp(maxVolume, 0, timePassed/fadeTime);
+                timePassed += Time.unscaledDeltaTime;       // In case we choose to freeze gameplay with Time.timeScale = 0 but still fade
+                yield return null;
+            }
+        }
+    }
+
 }
 
 [System.Serializable]
