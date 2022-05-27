264361d4e13e6a27132c4d88d2d67a29f27ba1d0
Peter Sutarjo <peter.sutarjo@gmail.com>
2022-05-27 04:03:51 -0700

    added sound to start menu

--- Project Ronin/Assets/PressAnyButton.cs
+++ Project Ronin/Assets/PressAnyButton.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PressAnyButton : MonoBehaviour
 {
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip startMenu;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent(typeof(AudioSource)) as AudioSource;
+    } 
+
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
         {
+            audioSource.PlayOneShot(startMenu, 1f);
             SceneLoader.instance.LoadScene();
         }
     }
